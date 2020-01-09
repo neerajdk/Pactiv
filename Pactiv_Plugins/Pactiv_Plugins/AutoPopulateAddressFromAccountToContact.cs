83a222ace45d98abf842d751438dd5be36686a3e
Neeraj <Neeraj@DESKTOP-P6TE05Q>
2020-01-09 13:59:20 +0530

    added summary in the file AutoPopulateAddressFromAccountToContact.cs

--- Pactiv_Plugins/Pactiv_Plugins/AutoPopulateAddressFromAccountToContact.cs
+++ Pactiv_Plugins/Pactiv_Plugins/AutoPopulateAddressFromAccountToContact.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Pactiv_Plugins
 {
+    /// <summary>
+    /// Automatically populated the address details from Account entity to Contact entity based on selected Parent Account field.
+    /// </summary>
     public class AutoPopulateAddressFromAccountToContact
     {
 
