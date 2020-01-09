9b875c07e6df436accb9de46ece6338a07ca10ae
Neeraj <Neeraj@DESKTOP-P6TE05Q>
2020-01-09 14:29:23 +0530

    implemented the plugins interface and started writing first plugin class.

--- Pactiv_Plugins/Pactiv_Plugins/AutoPopulateAddressFromAccountToContact.cs
+++ Pactiv_Plugins/Pactiv_Plugins/AutoPopulateAddressFromAccountToContact.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
 
 namespace Pactiv_Plugins
 {
     /// <summary>
     /// Automatically populated the address details from Account entity to Contact entity based on selected Parent Account field.
     /// </summary>
-    public class AutoPopulateAddressFromAccountToContact
+    public class AutoPopulateAddressFromAccountToContact : IPlugin
     {
+        public AutoPopulateAddressFromAccountToContact(string _unSecureString)
+        {
+            try
+            {
 
+            }
+            catch (InvalidPluginExecutionException ex)
+            {
+
+            }
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+
+            }
+            catch (InvalidPluginExecutionException ex)
+            {
+
+            }
+        }
     }
 }
