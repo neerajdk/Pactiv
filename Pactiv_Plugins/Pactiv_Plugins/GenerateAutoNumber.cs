3e97b05a1d2e0b1b7bd9452e74b3b891f6b8bc76
Neeraj <Neeraj@DESKTOP-P6TE05Q>
2020-01-09 14:43:21 +0530

    created new plugin GenerateAutoNumber.cs, it will be used to generate the auto number for entities

---
+++ Pactiv_Plugins/Pactiv_Plugins/GenerateAutoNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace Pactiv_Plugins
+{
+    public class GenerateAutoNumber : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+
+            }
+            catch (InvalidPluginExecutionException ex)
+            {
+
+            }
+        }
+    }
+}
